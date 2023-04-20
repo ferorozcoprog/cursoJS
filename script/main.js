044873f3521a935bb52bb0286616f19722e5de6e
fernando <fernandoorozcoprog@gmail.com>
2023-04-19 22:49:56 -0300

    preentrega2 Orozco

--- script/main.js
+++ script/main.js
@@ -1,54 +1,67 @@
 alert("Bienvenido a la tienda del Real Madrid!! Solo por Hoy Descuentos 20% en camisas del segunda equipo");
 
-function preciototal(modelocamisa,cantidad,usuariosocio){
-    let precio;
-    switch (modelocamisa){
-        case "primera equipacion":
-            precio = 120;
-            break;
-        case "segunda equipacion":
-                precio = 100;
-            break;
-        case "tercera equipacion":
-                precio = 85;
-            break;
-        default:
-                return 0;
-    }
-    if(modelocamisa ==="segunda equipacion"){
-        precio = precio*0.8;
-    }
 
-    if(usuariosocio === true){
-        precio = precio*0.9;
-    }
+// Objeto con los precios de las camisas
+const preciosCamisas = {
+  "primera equipacion": 120,
+  "segunda equipacion": 100,
+  "tercera equipacion": 85,
+};
 
-    return precio*cantidad
-
+// Función para obtener el precio total de la compra
+function obtenerPrecioTotal(camisa, cantidad, esSocio) {
+  // Función de orden superior para calcular el descuento por el tipo de camisa
+  const calcularDescuentoCamisa = (precio) => {
+    let descuentoPrecio = precio;
+    if (camisa === "segunda equipacion") {
+        descuentoPrecio = precio * 0.8;
     }
+    return descuentoPrecio;
+}
 
-    let modelovalido = false;
-    let modelocamisa;
-    while (!modelovalido){
-        modelocamisa = prompt("Por favor ingrese la camisa que desea comprar (primera equipacion, segunda equipacion, tercer equipacion)");
-        if (modelocamisa === "primera equipacion" || modelocamisa === "segunda equipacion" || modelocamisa ==="tercera equipacion") {
-            modelovalido = true;
-        }
-        else {
-            alert("El modelo ingresado no es correcto, ingresa una de las opciones mencionadas");
-        }
+  // Función de orden superior para calcular el descuento por ser socio
+  const calcularDescuentoSocio = (precio) => {
+    let descuentoPrecio = precio;
+    if (esSocio) {
+        descuentoPrecio = precio * 0.9;
     }
-    
-    let cantidad = parseInt(prompt("por favor ingresa la cantidad que desea comprar"));
-    let usuariosocio = false;
+    return descuentoPrecio;
+  }
 
-    let respuesta = prompt("Es socio de la pagina? Responda si o no");
-    if( respuesta !== null){
-        respuesta = respuesta.toLowerCase();
-        if(respuesta === "sí" || respuesta === "si"){
-            usuariosocio = true;
-            alert("Por ser socio tienes un 10% adicional");
-        }
-    }
-let totalpagar = preciototal(modelocamisa,cantidad,usuariosocio);
-alert("el precio total de tu compra es: " + totalpagar);
+  let precio = preciosCamisas[camisa];
+  if (!precio) {
+    return 0;
+  }
+
+  precio = calcularDescuentoCamisa(precio);
+  precio = calcularDescuentoSocio(precio);
+
+  return precio * cantidad;
+}
+
+let modeloValido = false;
+let modelocamisa;
+
+while (!modeloValido) {
+  modelocamisa = prompt("Por favor ingrese la camisa que desea comprar (primera equipacion, segunda equipacion, tercera equipacion)");
+  if (Object.keys(preciosCamisas).includes(modelocamisa)) {
+    modeloValido = true;
+  } else {
+    alert("El modelo ingresado no es correcto, ingresa una de las opciones mencionadas");
+  }
+}
+
+let cantidad = parseInt(prompt("por favor ingresa la cantidad que desea comprar"));
+let esSocio = false;
+
+let respuesta = prompt("Es socio de la pagina? Responda si o no");
+if (respuesta !== null) {
+  respuesta = respuesta.toLowerCase();
+  if (respuesta === "sí" || respuesta === "si") {
+    esSocio = true;
+    alert("Por ser socio tienes un 10% adicional");
+  }
+}
+
+let totalPagar = obtenerPrecioTotal(modelocamisa, cantidad, esSocio);
+alert("el precio total de tu compra es: " + totalPagar);
