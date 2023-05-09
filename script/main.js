2036db0bea48cb15de43c07a1a4933409c7afc5b
fernando <fernandoorozcoprog@gmail.com>
2023-05-09 01:02:16 -0300

    idea orozco

--- script/main.js
+++ script/main.js
@@ -16,3 +16,45 @@ function guardarInformacion() {
   alert("Datos guardados correctamente");
 }
 
+const camisetas = [
+{
+  "tipo": "Camiseta de la primera equipación",
+  "descripcion": "Esta camiseta oficial de la primera equipación del Real Madrid luce un look atemporal con detalles en morado claro y el escudo del club repetido sobre un fondo blanco.",
+  "precio": 120
+},
+{
+  "tipo": "Camiseta de la segunda equipación",
+  "descripcion": "Esta camiseta adidas de la segunda equipación del Real Madrid luce una nueva combinación de colores. Material 100% reciclado",
+  "precio": 100
+},
+{
+  "tipo": "Camiseta de la tercera equipación",
+  "descripcion": "Esta camiseta oficial adidas del Real Madrid se inspira en la nueva estética del Bernabéu",
+  "precio": 85
+},
+];
+
+const seccionPrecio = document.getElementById("seccionPrecio");
+
+const btnPrimera = document.getElementById("botonPrimera");
+btnPrimera.addEventListener("click", function() {
+  actualizarPrecio(0);
+});
+
+const btnSegunda = document.getElementById("botonSegunda");
+btnSegunda.addEventListener("click", function() {
+  actualizarPrecio(1);
+});
+
+const btnTercera = document.getElementById("botonTercera");
+btnTercera.addEventListener("click", function() {
+  actualizarPrecio(2);
+});
+
+function actualizarPrecio(indiceCamiseta) {
+  const camiseta = camisetas[indiceCamiseta];
+  seccionPrecio.innerHTML = `
+    <p>Precio: $${camiseta.precio}</p>
+  `;
+}
+
