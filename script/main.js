b3602c46c04fbc55e4cbb8123281d52c5c306fa4
fernando <fernandoorozcoprog@gmail.com>
2023-05-25 23:57:52 -0300

    proyecto final orozco

--- script/main.js
+++ script/main.js
@@ -1,60 +1,10 @@
-
-function guardarInformacion() {
-  const nombre = document.getElementById("validationTooltip01").value;
-  const correo = document.getElementById("validationTooltip05").value;
-  const fechaNacimiento = document.getElementById("validationTooltip02").value;
-  const tipoSuscripcion = document.getElementById("validationTooltip04").value;
-  const pais = document.getElementById("validationTooltip03").value;
-  
-
-  localStorage.setItem("validationTooltip01", nombre);
-  localStorage.setItem("validationTooltip05", correo);
-  localStorage.setItem("validationTooltip02", fechaNacimiento);
-  localStorage.setItem("validationTooltip04", tipoSuscripcion);
-  localStorage.setItem("validationTooltip03", pais);
-
-  alert("Datos guardados correctamente");
+class camiseta {
+  constructor(nombre, precio, descripcion, stock, img) {
+    this.nombre = nombre;
+    this.precio = precio;
+    this.descripcion = descripcion;
+    this.stock= stock;
+    this.img = img;
+  }
 }
 
-const camisetas = [
-{
-  "tipo": "Camiseta de la primera equipación",
-  "descripcion": "Esta camiseta oficial de la primera equipación del Real Madrid luce un look atemporal con detalles en morado claro y el escudo del club repetido sobre un fondo blanco.",
-  "precio": 120
-},
-{
-  "tipo": "Camiseta de la segunda equipación",
-  "descripcion": "Esta camiseta adidas de la segunda equipación del Real Madrid luce una nueva combinación de colores. Material 100% reciclado",
-  "precio": 100
-},
-{
-  "tipo": "Camiseta de la tercera equipación",
-  "descripcion": "Esta camiseta oficial adidas del Real Madrid se inspira en la nueva estética del Bernabéu",
-  "precio": 85
-},
-];
-
-const seccionPrecio = document.getElementById("seccionPrecio");
-
-const btnPrimera = document.getElementById("botonPrimera");
-btnPrimera.addEventListener("click", function() {
-  actualizarPrecio(0);
-});
-
-const btnSegunda = document.getElementById("botonSegunda");
-btnSegunda.addEventListener("click", function() {
-  actualizarPrecio(1);
-});
-
-const btnTercera = document.getElementById("botonTercera");
-btnTercera.addEventListener("click", function() {
-  actualizarPrecio(2);
-});
-
-function actualizarPrecio(indiceCamiseta) {
-  const camiseta = camisetas[indiceCamiseta];
-  seccionPrecio.innerHTML = `
-  <h2>${camiseta.tipo}</h2>
-  <p>Precio: $${camiseta.precio}</p>
-`;
-}
